1ca17fbd0c69da4ecae11a9f9b5456703d76053b
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-05-07 17:32:02 +0200

    chore(redux): start updating redux-form

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -143,8 +143,7 @@ export default class Home extends Component {
             </dd>
             <dt>Forms</dt>
             <dd>
-              The <Link to="/survey">Survey page</Link> uses the
-              still-experimental <a href="https://github.com/erikras/redux-form" target="_blank">redux-form</a> to
+              The <Link to="/survey">Survey page</Link> uses <a href="https://github.com/erikras/redux-form" target="_blank">redux-form</a> to
               manage form state inside the Redux store. This includes immediate client-side validation.
             </dd>
             <dt>WebSockets / socket.io</dt>
